36c11cf590c19043ad649d93cd17c110e5805928
thiagoifrn <thiagoifrn@gmail.com>
2025-04-23 16:12:46 -0300

    feat: add about me

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { ButtonPrimaryComponent } from "../../shared/button-primary/button-primary.component";
 import { ImageHeaderComponent } from "../image-header/image-header.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, ButtonPrimaryComponent, ImageHeaderComponent],
+  imports: [HeaderComponent, ButtonPrimaryComponent, ImageHeaderComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
